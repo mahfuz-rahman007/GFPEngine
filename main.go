a14a96f3399d7ffa17524013f0979675f2789cb3
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2026-08-16 23:25:39 +0600

    Argument catching

--- main.go
+++ main.go
@@ -6,14 +6,25 @@ import (
 )
 
 func main() {
-	fmt.Println("GFPEngine starting")
+	fmt.Println("------GFPEngine Starting------")
 
 	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "No path given")
+		fmt.Fprintln(os.Stderr, "No Path Given")
 		os.Exit(1)
 	}
 
 	path := os.Args[1]
+	fmt.Println("Verifying Given Path " + path)
 
-	fmt.Println("Searching Directory " + path)
+	info, err := os.Stat(path)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Cannot Find "+path)
+		os.Exit(1)
+	}
+
+	if !info.IsDir() {
+		fmt.Fprintln(os.Stderr, path+" is a File Not a Valid Directory")
+		os.Exit(1)
+	}
 }
