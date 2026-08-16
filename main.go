ee58621f07f7051e0ccc030c55cca1fba28021a5
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2026-08-17 00:13:06 +0600

    flag added

--- main.go
+++ main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
 )
 
 func main() {
 	fmt.Println("------GFPEngine Starting------")
 
-	if len(os.Args) < 2 {
+	workers := flag.Int("workers", 4, "Number of Workers")
+	output := flag.String("output", "", "output json file")
+	
+	flag.Parse()
+
+	if flag.NArg() < 1 {
 		fmt.Fprintln(os.Stderr, "No Path Given")
 		os.Exit(1)
 	}
 
-	path := os.Args[1]
+	path := flag.Arg(0)
 	fmt.Println("Verifying Given Path " + path)
 
 	info, err := os.Stat(path)
-
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Cannot Find "+path)
 		os.Exit(1)
@@ -27,4 +32,7 @@ func main() {
 		fmt.Fprintln(os.Stderr, path+" is a File Not a Valid Directory")
 		os.Exit(1)
 	}
+
+	fmt.Println("Number of workers ", *workers)
+	fmt.Println("Ouput json file: ", *output)
 }
